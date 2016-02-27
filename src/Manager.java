4e95269900409d31da377b7252424c8beb5a796f
Joss-Steward <joss.steward@gmail.com>
2016-02-27 18:49:45 -0500

    Added a manager class and tests for it.

---
+++ src/Manager.java
@@ -0,0 +1,26 @@
+/**
+ * @author Joss Steward
+ *
+ */
+public class Manager {
+	/**
+	 * This method takes a string and returns the decimal 
+	 *  representation of the string.  If the string cannot be
+	 *  converted into a decimal, this will return 0. 
+	 * @param inputString
+	 * @return The decimal representation of the number in the string.
+	 */
+	public static double ConvertToDecimal(String inputString){
+		State currentState = new StartState();
+		
+		for (char character : inputString.toCharArray()){
+			currentState = currentState.evaluate(character);
+			
+			if(currentState instanceof EndState){
+				break;
+			}
+		}
+		
+		return currentState.getValue();
+	}
+}
---
+++ tests/TestManager.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Joss Steward
+ */
+public class TestManager {
+	/**
+	 * Test converting an integer string into an integer.
+	 */
+	@Test
+	public void testIntegerConversion() {
+		assertEquals(200, Manager.ConvertToDecimal("200"), 0.0001);
+	}
+	
+	/**
+	 * Test converting a decimal.
+	 */
+	@Test
+	public void testDecimalConversion() {
+		assertEquals(200.05, Manager.ConvertToDecimal("200.05"), 0.0001);
+	}
+	
+	/**
+	 * Test an invalid conversion.
+	 */
+	@Test
+	public void testInvalidConversion() {
+		assertEquals(0, Manager.ConvertToDecimal("200.05.0004"), 0.0001);
+	}
+	
+	/**
+	 * Test an invalid conversion.
+	 */
+	@Test
+	public void testInvalidConversion2() {
+		assertEquals(0, Manager.ConvertToDecimal("abc123"), 0.0001);
+	}
+
+	/**
+	 * Test an invalid conversion.
+	 */
+	@Test
+	public void testInvalidConversion3() {
+		assertEquals(0, Manager.ConvertToDecimal("123abc"), 0.0001);
+	}
+
+	/**
+	 * Test an invalid conversion.
+	 */
+	@Test
+	public void testInvalidConversion4() {
+		assertEquals(0, Manager.ConvertToDecimal(" 123"), 0.0001);
+	}
+}
